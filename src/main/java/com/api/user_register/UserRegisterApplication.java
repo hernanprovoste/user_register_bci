4a22a11b464389f1bab70f015585951d899c9d41
Hernan Provoste <hernanprovostev@outlook.com>
2025-12-18 13:28:12 -0300

    first commit

---
+++ src/main/java/com/api/user_register/UserRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.api.user_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegisterApplication.class, args);
+	}
+
+}
